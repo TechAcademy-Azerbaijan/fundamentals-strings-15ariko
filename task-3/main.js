9dd00ddd87ec9ded2bb14d68b9bca43d0303d4f4
Arif <110599821+15ariko@users.noreply.github.com>
2022-09-09 12:06:33 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,22 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+let num = result.input;
+  let bad = 0;
+  let good = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == 2) {
+      bad++;
+    } else if (num[i] == 5) {
+      good++;
+    }
+  }
+  if (bad > good) {
+    console.log("2");
+  } else if (good > bad) {
+    console.log("5");
+  } else if (good == bad) {
+    console.log(`=`);
+  }
   
 });
